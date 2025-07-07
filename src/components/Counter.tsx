8cfa00275969e3ea09380115fca3a7954521dc8b
winsonlin21 <winsonlin21@gmail.com>
2025-07-07 00:03:49 -0400

    finsihed checkpoint 3
    finsihed the counter tsx

---
+++ src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+type CounterProps = {
+  initial: number;
+};
+
+export default function Counter({ initial }: CounterProps) {
+  const [count, setCount] = useState(initial);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
