9cff91d88188ef583fd6d1e85fb73a5c65383a9e
winsonlin21 <winsonlin21@gmail.com>
2025-07-09 22:21:28 -0400

    Started nhost for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,62 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+'use client'
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import { NhostProvider } from "@nhost/nextjs";
+import { nhost } from '../lib/nhost'
+
+const getMovies = `
+  query {
+    movies {
+      title
+      genre
+      rating
+    }
+  }
+`;
+
+function App() {
   return (
-    <main>
-      <Link href="/about">
-        <Button>Go to About Page</Button>
-      </Link>
-    </main>
+    <NhostProvider nhost={nhost}>
+      <Home />
+    </NhostProvider>
   );
 }
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      setLoading(true);
+      const { data, error } = await nhost.graphql.request(getMovies);
+
+      setMovies(data.movies);
+      setLoading(false);
+    }
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table>
+          <tbody>
+            {movies.map((movie, index) => (
+              <tr key={index}>
+                <td>{movie.title}</td>
+                <td>{movie.genre}</td>
+                <td>{movie.rating}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/nhost.ts
@@ -0,0 +1,6 @@
+import { NhostClient } from "@nhost/nextjs";
+
+export const nhost = new NhostClient({
+  subdomain: "ermtgtppnukqwdeemgob",
+  region: "us-east-1",
+})
